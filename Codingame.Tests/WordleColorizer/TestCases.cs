58192d83f146580fe5cdfb24915b009037f5ff93
Arthur Cousseau <arthur.cousseau@me.com>
2025-05-20 23:58:04 +0200

    Add WordleColorizer

---
+++ Codingame.Tests/WordleColorizer/TestCases.cs
@@ -0,0 +1,34 @@
+﻿namespace Codingame.Tests.WordleColorizer
+{
+    public class TestCases
+    {
+        [TestCase("Polka", new string[] { "XXXXO", "XXOXX", "XOOXX", "#####" })]
+        [TestCase("Water", new string[] { "XXXXX", "OOXXO", "OOOXX", "###XX", "#####" })]
+        [TestCase("Koala", new string[] { "OOXOX", "OX#XX", "XX###", "OOXXX", "XX#X#", "#####" })]
+        [TestCase("Nanny", new string[] { "XOXXO", "#XXXO", "#OXXO", "#X##X", "#O##O", "#####" })]
+        public void TestFiles(string filename, string[] expected)
+        {
+            Console.Init(File.ReadAllLines($"WordleColorizer/TestFiles/{filename}.txt"));
+            string[] actual = Codingame.WordleColorizer.Main.GetResult();
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.That(actual[i], Is.EqualTo(expected[i]));
+            }
+        }
+
+        [TestCase("ABCDE", "ABCDE", "#####")] // Full win
+        [TestCase("ABCDE", "FFFFF", "XXXXX")] // Full fail
+        [TestCase("ABCDE", "FFCFF", "XX#XX")] // One letter win
+        [TestCase("ABCDE", "AFCFE", "#X#X#")] // Multiple letter win
+        [TestCase("ABCDE", "FFAFF", "XXOXX")] // One letter misplaced
+        [TestCase("ABCDE", "FAFCF", "XOXOX")] // Multiple letter misplaced
+        // Bongo (from the forum)
+        [TestCase("BONGO", "OOOOO", "X#XX#")]
+        [TestCase("BONGO", "ONGBA", "OOOOX")]
+        [TestCase("BONGO", "NNOGO", "OXO##")]
+        public void SingleCases(string answer, string attempt, string expected)
+        {
+            Assert.That(Codingame.WordleColorizer.Main.GetResult(answer, attempt), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Codingame/WordleColorizer/Main.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace Codingame.WordleColorizer
+{
+    public static class Main
+    {
+        public static string GetResult()
+        {
+            return string.Join("\n", GetResults());
+        }
+
+        public static string[] GetResults()
+        {
+            string answer = Console.ReadLine();
+            int N = int.Parse(Console.ReadLine());
+
+            string[] results = new string[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string attempt = Console.ReadLine();
+                results[i] = GetResult(answer, attempt);
+            }
+
+            return results;
+        }
+
+        public static string GetResult(string answer, string attempt)
+        {
+            Dictionary<char, int> counts = [];
+            char[] result = new char[attempt.Length];
+
+            // First pass: mark exact matches (#) and count unmatched answer letters
+            for (int i = 0; i < answer.Length; i++)
+            {
+                char c = answer[i];
+                if (c == attempt[i])
+                {
+                    result[i] = '#';
+                }
+                else
+                {
+                    if (counts.ContainsKey(c))
+                        counts[c]++;
+                    else
+                        counts[c] = 1;
+                }
+            }
+
+            for (int i = 0; i < attempt.Length; i++)
+            {
+                if (result[i] != 0)
+                    continue;
+
+                char c = attempt[i];
+                if (counts.ContainsKey(c) && counts[c] > 0)
+                {
+                    result[i] = 'O';
+                    counts[c]--;
+                }
+                else
+                {
+                    result[i] = 'X';
+                }
+            }
+
+            return string.Join("", result);
+        }
+    }
+}
